ecb3bfd286e9dedc43b6db78e0fb6d025fe4c222
layderdgs <laydesilva94@gmail.com>
2025-02-21 19:03:03 +0000

    commit final

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,7 +8,7 @@ int main() {
     int populacao, numero_de_pontos_turisticos;
     float area, PIB;
 
-    printf("Digite os dados da carta:\n\n")
+    printf("Digite os dados da carta:\n\n");
 
     printf("Digite o Estado:");
     scanf("%s", &estado);
@@ -30,9 +30,41 @@ int main() {
 
     printf("Digite a quantidade de pontos turísticos da Cidade:");
     scanf("%d", &numero_de_pontos_turisticos);
+     
+    printf("\nCarta 1:\n");
 
-   printf("Estado:%s,  Código:%s, Nome da Cidade:%s, População:%d, Área:%f, PIB:%f, Número de pontos turísticos:%d ", estado, codigo_da_carta, nome_da_cidade, populacao, area, PIB, numero_de_pontos_turisticos);
+   printf("\n Estado:%s\n Código:%s\n Nome da Cidade:%s\n População:%d\n Área:%f km²\n PIB:%f bilhões de reais\n Número de pontos turísticos:%d\n ",estado, codigo_da_carta, nome_da_cidade, populacao, area, PIB, numero_de_pontos_turisticos);
     
+   printf("\n\nCarta registrada com sucesso!\n");
+   printf("Digite os dados da próxima carta:\n");
+
+   printf("Digite o Estado:");
+   scanf("%s", &estado);
+
+   printf("Digite o código da carta:");
+   scanf("%s", &codigo_da_carta);
+
+   printf("Digite o nome da Cidade:");
+   scanf("%s",&nome_da_cidade);
+
+   printf("Digite o número de habitantes da Cidade:");
+   scanf("%d", &populacao);
+
+   printf("Digite a área da Cidade em km²:");
+   scanf("%f", &area);
+
+   printf("Digite o PIB da Cidade:");
+   scanf("%f", &PIB);
+
+   printf("Digite a quantidade de pontos turísticos da Cidade:");
+   scanf("%d", &numero_de_pontos_turisticos);
+    
+   printf("\nCarta 2:\n");
+
+   printf("\n Estado:%s\n Código:%s\n Nome da Cidade:%s\n População:%d\n Área:%f km²\n PIB:%f bilhões de reais\n Número de pontos turísticos:%d\n ",estado, codigo_da_carta, nome_da_cidade, populacao, area, PIB, numero_de_pontos_turisticos);
+    printf( "\nCarta registrada com sucesso!\n");
+   
+   
 return 0;
     
 }
