a8dc9c46280432dd4ba246c7e618781f7eeb187a
layderdgs <laydesilva94@gmail.com>
2025-02-21 18:01:43 +0000

    print final

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,37 +1,37 @@
 #include <stdio.h>
 
 int main() {
+
     printf("Cadastro de Cartas\n \n");
     
     char estado[8], codigo_da_carta[70], nome_da_cidade[70];
     int populacao, numero_de_pontos_turisticos;
     float area, PIB;
 
-    printf("Carta 1:\n \n");
-     
+    printf("Digite os dados da carta:\n\n")
+
     printf("Digite o Estado:");
-    scanf("%s", estado);
+    scanf("%s", &estado);
 
     printf("Digite o código da carta:");
-    scanf("%s", codigo_da_carta);
+    scanf("%s", &codigo_da_carta);
 
     printf("Digite o nome da Cidade:");
-    scanf("%s",nome_da_cidade);
+    scanf("%s",&nome_da_cidade);
  
     printf("Digite o número de habitantes da Cidade:");
-    scanf("%d", populacao);
+    scanf("%d", &populacao);
 
     printf("Digite a área da Cidade em km²:");
-    scanf("%d", area);
+    scanf("%f", &area);
 
     printf("Digite o PIB da Cidade:");
-    scanf("%f",PIB);
+    scanf("%f", &PIB);
 
-    printf("Digite a quantiidade de pontos turísticos dda Cidade:");
-    scanf("%d", numero_de_pontos_turisticos);
+    printf("Digite a quantidade de pontos turísticos da Cidade:");
+    scanf("%d", &numero_de_pontos_turisticos);
 
-
-
+   printf("Estado:%s,  Código:%s, Nome da Cidade:%s, População:%d, Área:%f, PIB:%f, Número de pontos turísticos:%d ", estado, codigo_da_carta, nome_da_cidade, populacao, area, PIB, numero_de_pontos_turisticos);
     
 return 0;
     
